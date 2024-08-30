cf57f917f05255d6b981dde7e553bd22cfd57aac
RajeshTanguturi <rajeshtanguturi31@gmail.com>
2024-08-30 15:01:16 +0530

    jwt done

---
+++ frontend/src/components/FileUpload.jsx
@@ -0,0 +1,179 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/fileupload.scss";
+
+// import "../styles/toggle.scss";
+
+const FileUpload = () => {
+  const [image, setImage] = useState(null);
+  const [previewUrl, setPreviewUrl] = useState(null); 
+  const [patientDetails, setpatientDetails] = useState({
+    patientName: "",
+    patientAge: "",
+    patientSex: "",
+  });
+  const { patientName, patientAge, patientSex } = patientDetails;
+  const [loading, setLoading] = useState(false);
+
+  const submitImage = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const formData = new FormData();
+    formData.append("image", image);
+    formData.append("patientName", patientName);
+    formData.append("patientAge", patientAge);
+    formData.append("patientSex", patientSex);
+
+    console.log(formData);
+
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/image/uploadimage",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      toast.success("Data uploaded successfully");
+      console.log("Response from server:", response.data);
+
+      setpatientDetails({
+        patientName: "",
+        patientAge: "",
+        patientSex: "",
+      });
+      setImage(null);
+    } catch (error) {
+      toast.error("Error uploading data");
+      console.error("Error uploading image:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const uploadImage = (e) => {
+    const file = e.target.files[0];
+    setImage(file);
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreviewUrl(reader.result); // Set the preview URL
+      };
+      reader.readAsDataURL(file);
+    } else {
+      setPreviewUrl(null); // Reset if no file is selected
+    }
+  };
+
+  const updatePatientDetails = (e) => {
+    const { name, value } = e.target;
+    setpatientDetails((prevDetails) => ({
+      ...prevDetails,
+      [name]: value,
+    }));
+    console.log(e);
+    console.log(e.target.value);
+  };
+
+  const isFormComplete = patientName && patientAge && patientSex && image;
+  return (
+    <div>
+      <div className="fileupload">
+        <div className="card">
+          <form onSubmit={submitImage}>
+            <div className="form-floating">
+              <input
+                maxLength="12"
+                type="text"
+                class="form-control"
+                id="floatingInputpname"
+                name="patientName"
+                value={patientName}
+                placeholder="name"
+                onChange={updatePatientDetails}
+                required
+              />
+              <label htmlFor="floatingInputpname">Patient's Name</label>
+            </div>
+
+            <div className="form-floating">
+              <input
+                maxLength="3"
+                type="number"
+                class="form-control"
+                id="floatingInputpage"
+                name="patientAge"
+                value={patientAge}
+                placeholder="Age"
+                onChange={updatePatientDetails}
+                required
+              />
+              <label htmlFor="floatingInputpage">Age</label>
+            </div>
+
+            <div className="form-floating">
+              <select
+                className="form-select"
+                id="floatingSelectpsex"
+                name="patientSex"
+                aria-label="Floating label select example"
+                value={patientSex}
+                onChange={updatePatientDetails}
+                required
+              >
+                <option value="" disabled>
+                  Select gender
+                </option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+              </select>
+              <label htmlFor="floatingSelectpsex"></label>
+            </div>
+            <div class="mb-3">
+              <label for="formFile" class="form-label">
+                Select X-ray image
+              </label>
+              <input
+                class="form-control"
+                type="file"
+                id="formFile"
+                accept="image/*"
+                onChange={uploadImage}
+              />
+            </div>
+
+            {previewUrl && (
+              <div className="image-preview">
+                <img src={previewUrl} alt="Selected" style={{ maxWidth: '100%', height: 'auto', marginTop: '10px' }} />
+              </div>
+            )}
+      
+            <div>
+              {loading ? (
+                <p>loading...</p>
+              ) : (
+                isFormComplete && (
+                  <div>
+                    <button type="submit" className="btn btn-success">
+                      submit
+                    </button>
+                  </div>
+                )
+              )}
+            </div>
+          </form>
+        </div>
+      </div>
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default FileUpload;
---
+++ frontend/src/pages/User.jsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import axios from "axios";
+import Header from "../components/Header";
+import { ToastContainer, toast } from "react-toastify";
+import "../styles/user.scss";
+import FileUpload from "../components/FileUpload";
+import "../styles/dropdown.scss";
+import Addinfo from "../components/Addinfo";
+
+const User = () => {
+  const navigate = useNavigate();
+  const [cookies, removeCookie] = useCookies([]);
+  const [username, setUsername] = useState("");
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const handleChange = (e) => {
+    setSelectedOption(e.target.value);
+  };
+  useEffect(() => {
+    const verifyCookie = async () => {
+      if (!cookies.token) {
+        navigate("/login");
+      }
+      const { data } = await axios.post(
+        "http://localhost:4000/auth",
+        {},
+        { withCredentials: true }
+      );
+      const { status, user } = data;
+      setUsername(user);
+      return status
+        ? toast(`Hello ${user}`, {
+            position: "top-right",
+          })
+        : (removeCookie("token"), navigate("/login"));
+    };
+    verifyCookie();
+  }, [cookies, navigate, removeCookie]);
+
+  const handleLogout = () => {
+    removeCookie("token");
+    navigate("/signup");
+  };
+  return (
+    <>
+      <Header logout={handleLogout} />
+      <div className="home_page">
+        <h4>
+          {" "}
+          Welcome back <span>{username.toUpperCase()}</span>
+        </h4>
+        
+
+        <FileUpload />
+        {/* <Addinfo /> */}
+
+        {/* <button onClick={Logout}>LOGOUT</button> */}
+      </div>
+      {/* <ToastContainer /> */}
+    </>
+  );
+};
+export default User;
